9919d94986a12244033d953d8a453faa4af2328b
Menchik1 <162079424+Menchik1@users.noreply.github.com>
2024-11-22 09:45:08 +0300

    Update locker.h

--- locker.h
+++ locker.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <sys/stat.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <cstring>
+#include "json.hpp" 
+#include <sstream> 
 #include "structures.h"
+#include "WorkWithFiles.h"
 
 using namespace std;
+using json = nlohmann::json;
 
 void lockPrimaryKey(dbase& db);
 void unlockPrimaryKey(dbase& db);
 void initializePrimaryKey(dbase& db);
 void updatePrimaryKey(dbase& db);
-
