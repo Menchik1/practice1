3784b069def5b698d1f50385ccbfa96fdc462961
Menchik1 <162079424+Menchik1@users.noreply.github.com>
2024-11-22 09:43:39 +0300

    Update commands.h

--- commands.h
+++ commands.h
@@ -1,14 +1,22 @@
 #pragma once
-
 #include <iostream>
+#include <fstream>
+#include <sys/stat.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <cstring> 
+#include "json.hpp" 
+#include <sstream> 
 #include "structures.h"
-#include "json.hpp"
 #include "locker.h"
-#include "fileOperations.h"
-using namespace std;
 
+using namespace std;
+using json = nlohmann::json;
+
+bool applyFilter(const json& entry, const Pars<string, string>& filter);
+void selectFromMultipleTables(dbase& db, const string& column, const string& column2, const string& table1, const string& table2, const Pars<string, string>& filter1, const Pars<string, string>& filter2, const string& WHERE, const string& filter_type, const string& tablef, stringstream& result_stream);
+void selectFromTable(dbase& db, const string& table, const Pars<string, string>& filter);
 void insert(dbase& db, const string& table, json entry);
-void select(dbase& db, const string& column, const string& table, const Pars<string, string> filters[], int filter_count, const string& filter_type);
-void Delete(dbase& db, const string& column, const string& value, const string& table);
-bool applyAndFilters(const json& entry, const Pars<string, string> filters[], int filter_count);
-bool applyOrFilters(const json& entry, const Pars<string, string> filters[], int filter_count);
+void deleteRow(dbase& db, const string& column, const string& value, const string& table);
